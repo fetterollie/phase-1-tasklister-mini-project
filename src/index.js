34f3daaa6fd58cd413149b11484d6e1803d7f01e
Jonathan <jonathan.fetterolf@gmail.com>
2023-08-01 14:27:28 -0400

    complete functionality of tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const buildButton = (e) => {
+    const taskList = document.querySelector('#tasks')
+    const todoText = e.target.querySelector('#new-task-description').value;
+    const newTask = document.createElement("li");
+    const deleteButton = document.createElement("button")
+
+    newTask.innerText = `${todoText} `;
+    deleteButton.innerText = 'x';
+    deleteButton.id = 'delete-button'
+
+    newTask.appendChild(deleteButton);
+    taskList.appendChild(newTask);
+
+    // make delete button delete
+    deleteButton.addEventListener('click', () => {
+      deleteButton.parentElement.remove();
+    })
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    buildButton(e);
+    console.log(document.querySelector('#delete-button'))
+  };
+
+  document.querySelector('#create-task-form').addEventListener('submit', (e) => {
+    handleSubmit(e);
+  })
+
+
+
 });
